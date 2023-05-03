c6fffc2e6c8a0dcab88b1e365da4ffbac2323b64
MrGibby5257 <bpg5257@psu.edu>
2023-05-02 20:55:09 -0400

    final changes

---
+++ src/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<h1>🌜♓🅰🌴 🅱⚽🌴</h1>
+  {#if $username}
+    <div class="user-bio">
+      
+      <span>Hello <strong>{$username}</strong></span>
+      <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
+    </div>
+
+    <button class="signout-button" on:click={signout}>Sign Out</button>
+
+
+  {/if}
+</header>
